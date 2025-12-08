17cebbb93a128b560d40d105c5e04936de62adf2
CHANDRASHEKHAR PATIL <chandrashekhar.patil@heromotocorp.com>
2025-12-08 19:46:12 +0530

    code formatted using prettier

--- concepts/script_scope/script1.js
+++ concepts/script_scope/script1.js
@@ -1,3 +1,2 @@
-
-export const script1Variable = 'This is script1.js';
-console.log('This is script1 variable', script1Variable);
+export const script1Variable = "This is script1.js";
+console.log("This is script1 variable", script1Variable);
--- concepts/script_scope/script2.js
+++ concepts/script_scope/script2.js
@@ -1,6 +1,6 @@
-import { script1Variable } from './script1.js';
+import { script1Variable } from "./script1.js";
 
-export const script2Variable = 'This is script2.js';
-console.log('This is script2 variable', script2Variable);
+export const script2Variable = "This is script2.js";
+console.log("This is script2 variable", script2Variable);
 script1Variable = "Changing script1Variable";
-console.log('This is script1 variable', script1Variable);
+console.log("This is script1 variable", script1Variable);
