ac615d6f2b6c61ccba494ec06f8341629b9d1df3
CHANDRASHEKHAR PATIL <chandrashekhar.patil@heromotocorp.com>
2026-01-18 11:16:41 +0530

    merge_array

--- coding/merge_arrays.js
+++ coding/merge_arrays.js
@@ -2,6 +2,23 @@ const x = [{ a: 1 }, { b: 5 }];
 const y = [{ b: 2 }, { c: 0 }];
 // const outp = { a: 1, b: 5, c: 0 };
 
+const first = x.reduce((acc, cur) => {
+  const key = Object.keys(cur)[0];
+  acc[key] = cur[key];
+  return acc;
+}, {});
+
+const final = y.reduce((acc, cur) => {
+  const key = Object.keys(cur)[0];
+  if (acc[key] === undefined) {
+    acc[key] = cur[key];
+  }
+
+  return acc;
+}, first);
+
+console.log("final", final);
+
 const mergeProperties = (x, y) => {
   const outp = {};
   x.forEach((item) => {
